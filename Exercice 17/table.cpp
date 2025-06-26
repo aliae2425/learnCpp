dc284df45b7912736277f54593dbecb005effc3b
aliae2425 <noam.carmi80@gmail.com>
2025-06-26 22:38:17 +0200

    Refactor code structure for improved readability and maintainability

---
+++ Exercice 17/table.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+
+std::vector<int> createTable(int size) {
+    std::vector<int> table(size);
+    for (int i = 0; i < size; ++i) {
+        table[i] = rand() % 100; // Random numbers between 0 and 99
+    }
+    return table;
+}
+
+// void displayTable(const std::vector<int>& table) {
+void displayTable(std::vector<int> &table) {
+    std::cout << "index |";
+    for (size_t i = 0; i < table.size(); ++i) {
+        std::cout << std::setw(4) << i + 1 << " |";
+    }
+    std::cout << std::endl;
+    std::cout << "value |";
+    for (size_t i = 0; i < table.size(); ++i) {
+        std::cout << std::setw(4) << table[i] << " |";
+    }
+    std::cout << std::endl;
+}
+
+int average(const std::vector<int>& table) {
+    int sum = 0;
+    for (int value : table) {
+        sum += value;
+    }
+    return sum / table.size();
+}
+
+int main(){
+    // Initialiser la seed pour les nombres aléatoires
+    srand(time(nullptr));
+    
+    int tableSize;
+    std::cout << "entrez la taille de la table: ";
+    std::cin >> tableSize;
+    if (tableSize <= 0) {
+        std::cout << "Veuillez entrer un nombre entier positif." << std::endl;
+        return 1; // Exit with an error code
+    }
+
+
+    std::vector<int> table = createTable(tableSize);
+    displayTable(table);
+
+    std::cout <<std::endl << std::endl << "La moyenne de la table est: " << average(table) << std::endl;
+
+    return 0;
+}
